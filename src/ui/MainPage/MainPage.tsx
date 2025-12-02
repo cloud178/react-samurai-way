9db3e6efdd98f09c720f97ac46006c7231619f2d
Denis Biryukov <denis-biryukov-13@mail.ru>
2025-12-02 23:34:52 +0300

    add title

--- src/ui/MainPage/MainPage.tsx
+++ src/ui/MainPage/MainPage.tsx
@@ -11,6 +11,7 @@ export function MainPage() {
   return (
     <div>
       <div style={{ display: 'flex', gap: '40px' }}>
+        <h1>Musicfun Player</h1>
         <TracksList onTrackSelect={handleTrackSelect} selectedTrackId={trackId} />
         <TrackDetail trackId={trackId} />
       </div>
